3ba8e28721a81fb9fc582f03479bd6ea18d794f6
Anushka <anushkakohli2021@gmail.com>
2024-07-11 08:27:20 +0530

    navbar init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div>
+            {/* Upper nav */}
+            <div>
+                <h1>Navbar</h1>
+            </div>
+            {/* Lower nav */}
+            <div>
+                <h2>Subnav</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
